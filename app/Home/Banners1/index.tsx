76536a652d34ca11594307273671ce4298bd9cc2
Al Sajid <alsajiduzzaman@outlook.com>
2023-08-09 16:47:19 +0600

    focusing

--- app/Home/Banners1/index.tsx
+++ app/Home/Banners1/index.tsx
@@ -1,94 +1,94 @@
-import Image from 'next/image'
+// import Image from 'next/image'
 
-export default function Banners1() {
-	return (
-		<section className='banner__section section--padding'>
-			<div className='container-fluid'>
-				<div className='row mb--n28'>
-					<div className='col-lg-5 col-md-order mb-28'>
-						<div className='banner__items'>
-							<a className='banner__items--thumbnail position__relative' href='shop.html'>
-								<Image className='banner__items--thumbnail__img' src='/images/banners/banner1.png' alt='banner-img' width={620} height={688} />
-								<div className='banner__items--content'>
-									<span className='banner__items--content__subtitle'>17% Discount</span>
-									<h2 className='banner__items--content__title h3'>
-										Spring Collection <br />
-										Style To
-									</h2>
-									<span className='banner__items--content__link'>
-										View Discounts
-										<svg className='banner__items--content__arrow--icon' xmlns='http://www.w3.org/2000/svg' width='20.2' height='12.2' viewBox='0 0 6.2 6.2'>
-											<path d='M7.1,4l-.546.546L8.716,6.713H4v.775H8.716L6.554,9.654,7.1,10.2,9.233,8.067,10.2,7.1Z' transform='translate(-4 -4)' fill='currentColor'></path>
-										</svg>
-									</span>
-								</div>
-							</a>
-						</div>
-					</div>
-					<div className='col-lg-7 mb-28'>
-						<div className='row row-cols-lg-2 row-cols-sm-2 row-cols-1'>
-							<div className='col mb-28'>
-								<div className='banner__items'>
-									<a className='banner__items--thumbnail position__relative' href='shop.html'>
-										<Image className='banner__items--thumbnail__img' src='/images/banners/banner2.png' alt='banner-img' width={425} height={337} />
-										<div className='banner__items--content'>
-											<span className='banner__items--content__subtitle text__secondary'>Shop Women</span>
-											<h2 className='banner__items--content__title h3'>
-												Up to 70% Off & <br />
-												Free Shipping
-											</h2>
-											<span className='banner__items--content__link'>
-												View Discounts
-												<svg className='banner__items--content__arrow--icon' xmlns='http://www.w3.org/2000/svg' width='20.2' height='12.2' viewBox='0 0 6.2 6.2'>
-													<path d='M7.1,4l-.546.546L8.716,6.713H4v.775H8.716L6.554,9.654,7.1,10.2,9.233,8.067,10.2,7.1Z' transform='translate(-4 -4)' fill='currentColor'></path>
-												</svg>
-											</span>
-										</div>
-									</a>
-								</div>
-							</div>
-							<div className='col mb-28'>
-								<div className='banner__items'>
-									<a className='banner__items--thumbnail position__relative' href='shop.html'>
-										<Image className='banner__items--thumbnail__img' src='/images/banners/banner3.png' alt='banner-img' width={426} height={337} />
-										<div className='banner__items--content'>
-											<span className='banner__items--content__subtitle'>Shop Women</span>
-											<h2 className='banner__items--content__title h3'>
-												Free Shipping Over <br />
-												Order $120
-											</h2>
-											<span className='banner__items--content__link'>
-												View Discounts
-												<svg className='banner__items--content__arrow--icon' xmlns='http://www.w3.org/2000/svg' width='20.2' height='12.2' viewBox='0 0 6.2 6.2'>
-													<path d='M7.1,4l-.546.546L8.716,6.713H4v.775H8.716L6.554,9.654,7.1,10.2,9.233,8.067,10.2,7.1Z' transform='translate(-4 -4)' fill='currentColor'></path>
-												</svg>
-											</span>
-										</div>
-									</a>
-								</div>
-							</div>
-						</div>
-						<div className='banner__items'>
-							<a className='banner__items--thumbnail position__relative' href='shop.html'>
-								<Image className='banner__items--thumbnail__img banner__img--max__height' src='/images/banners/banner4.png' alt='banner-img' width={881} height={322} />
-								<div className='banner__items--content'>
-									<span className='banner__items--content__subtitle'>25% Discount</span>
-									<h2 className='banner__items--content__title h3'>
-										Leather Saddle <br />
-										Bag Style
-									</h2>
-									<span className='banner__items--content__link'>
-										View Discounts
-										<svg className='banner__items--content__arrow--icon' xmlns='http://www.w3.org/2000/svg' width='20.2' height='12.2' viewBox='0 0 6.2 6.2'>
-											<path d='M7.1,4l-.546.546L8.716,6.713H4v.775H8.716L6.554,9.654,7.1,10.2,9.233,8.067,10.2,7.1Z' transform='translate(-4 -4)' fill='currentColor'></path>
-										</svg>
-									</span>
-								</div>
-							</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-	)
-}
+// export default function Banners1() {
+// 	return (
+// 		<section className='banner__section section--padding'>
+// 			<div className='container-fluid'>
+// 				<div className='row mb--n28'>
+// 					<div className='col-lg-5 col-md-order mb-28'>
+// 						<div className='banner__items'>
+// 							<a className='banner__items--thumbnail position__relative' href='shop.html'>
+// 								<Image className='banner__items--thumbnail__img' src='/images/banners/banner1.png' alt='banner-img' width={620} height={688} />
+// 								<div className='banner__items--content'>
+// 									<span className='banner__items--content__subtitle'>17% Discount</span>
+// 									<h2 className='banner__items--content__title h3'>
+// 										Spring Collection <br />
+// 										Style To
+// 									</h2>
+// 									<span className='banner__items--content__link'>
+// 										View Discounts
+// 										<svg className='banner__items--content__arrow--icon' xmlns='http://www.w3.org/2000/svg' width='20.2' height='12.2' viewBox='0 0 6.2 6.2'>
+// 											<path d='M7.1,4l-.546.546L8.716,6.713H4v.775H8.716L6.554,9.654,7.1,10.2,9.233,8.067,10.2,7.1Z' transform='translate(-4 -4)' fill='currentColor'></path>
+// 										</svg>
+// 									</span>
+// 								</div>
+// 							</a>
+// 						</div>
+// 					</div>
+// 					<div className='col-lg-7 mb-28'>
+// 						<div className='row row-cols-lg-2 row-cols-sm-2 row-cols-1'>
+// 							<div className='col mb-28'>
+// 								<div className='banner__items'>
+// 									<a className='banner__items--thumbnail position__relative' href='shop.html'>
+// 										<Image className='banner__items--thumbnail__img' src='/images/banners/banner2.png' alt='banner-img' width={425} height={337} />
+// 										<div className='banner__items--content'>
+// 											<span className='banner__items--content__subtitle text__secondary'>Shop Women</span>
+// 											<h2 className='banner__items--content__title h3'>
+// 												Up to 70% Off & <br />
+// 												Free Shipping
+// 											</h2>
+// 											<span className='banner__items--content__link'>
+// 												View Discounts
+// 												<svg className='banner__items--content__arrow--icon' xmlns='http://www.w3.org/2000/svg' width='20.2' height='12.2' viewBox='0 0 6.2 6.2'>
+// 													<path d='M7.1,4l-.546.546L8.716,6.713H4v.775H8.716L6.554,9.654,7.1,10.2,9.233,8.067,10.2,7.1Z' transform='translate(-4 -4)' fill='currentColor'></path>
+// 												</svg>
+// 											</span>
+// 										</div>
+// 									</a>
+// 								</div>
+// 							</div>
+// 							<div className='col mb-28'>
+// 								<div className='banner__items'>
+// 									<a className='banner__items--thumbnail position__relative' href='shop.html'>
+// 										<Image className='banner__items--thumbnail__img' src='/images/banners/banner3.png' alt='banner-img' width={426} height={337} />
+// 										<div className='banner__items--content'>
+// 											<span className='banner__items--content__subtitle'>Shop Women</span>
+// 											<h2 className='banner__items--content__title h3'>
+// 												Free Shipping Over <br />
+// 												Order $120
+// 											</h2>
+// 											<span className='banner__items--content__link'>
+// 												View Discounts
+// 												<svg className='banner__items--content__arrow--icon' xmlns='http://www.w3.org/2000/svg' width='20.2' height='12.2' viewBox='0 0 6.2 6.2'>
+// 													<path d='M7.1,4l-.546.546L8.716,6.713H4v.775H8.716L6.554,9.654,7.1,10.2,9.233,8.067,10.2,7.1Z' transform='translate(-4 -4)' fill='currentColor'></path>
+// 												</svg>
+// 											</span>
+// 										</div>
+// 									</a>
+// 								</div>
+// 							</div>
+// 						</div>
+// 						<div className='banner__items'>
+// 							<a className='banner__items--thumbnail position__relative' href='shop.html'>
+// 								<Image className='banner__items--thumbnail__img banner__img--max__height' src='/images/banners/banner4.png' alt='banner-img' width={881} height={322} />
+// 								<div className='banner__items--content'>
+// 									<span className='banner__items--content__subtitle'>25% Discount</span>
+// 									<h2 className='banner__items--content__title h3'>
+// 										Leather Saddle <br />
+// 										Bag Style
+// 									</h2>
+// 									<span className='banner__items--content__link'>
+// 										View Discounts
+// 										<svg className='banner__items--content__arrow--icon' xmlns='http://www.w3.org/2000/svg' width='20.2' height='12.2' viewBox='0 0 6.2 6.2'>
+// 											<path d='M7.1,4l-.546.546L8.716,6.713H4v.775H8.716L6.554,9.654,7.1,10.2,9.233,8.067,10.2,7.1Z' transform='translate(-4 -4)' fill='currentColor'></path>
+// 										</svg>
+// 									</span>
+// 								</div>
+// 							</a>
+// 						</div>
+// 					</div>
+// 				</div>
+// 			</div>
+// 		</section>
+// 	)
+// }
--- app/Home/Slider/index.tsx
+++ app/Home/Slider/index.tsx
@@ -1,108 +1,108 @@
-import Image from 'next/image'
+// import Image from 'next/image'
 
-export default function Slider() {
-	return (
-		<section className='hero__slider--section'>
-			<div className='hero__slider--inner hero__slider--activation swiper'>
-				<div className='hero__slider--wrapper swiper-wrapper'>
-					<div className='swiper-slide '>
-						<div className='hero__slider--items home1__slider--bg'>
-							<div className='container-fluid'>
-								<div className='hero__slider--items__inner'>
-									<div className='row row-cols-1'>
-										<div className='col'>
-											<div className='slider__content'>
-												<p className='slider__content--desc desc1 mb-15'>
-													<Image className='slider__text--shape__icon' src='/images/icons/text-shape-icon.png' alt='text-shape-icon' width={25} height={21} /> New Collection
-												</p>
-												<h2 className='slider__content--maintitle h1'>
-													The Great Fashion <br />
-													Collection 2022
-												</h2>
-												<p className='slider__content--desc desc2 d-sm-2-none mb-40'>
-													Up To 40% Off Final Sale Items. <br />
-													Caught in the Moment!
-												</p>
-												<a className='slider__btn primary__btn' href='shop.html'>
-													Show Collection
-													<svg className='primary__btn--arrow__icon' xmlns='http://www.w3.org/2000/svg' width='20.2' height='12.2' viewBox='0 0 6.2 6.2'>
-														<path d='M7.1,4l-.546.546L8.716,6.713H4v.775H8.716L6.554,9.654,7.1,10.2,9.233,8.067,10.2,7.1Z' transform='translate(-4 -4)' fill='currentColor'></path>
-													</svg>
-												</a>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div className='swiper-slide '>
-						<div className='hero__slider--items home1__slider--bg two'>
-							<div className='container-fluid'>
-								<div className='row'>
-									<div className='col'>
-										<div className='hero__slider--items__inner'>
-											<div className='slider__content'>
-												<p className='slider__content--desc desc1 mb-15'>
-													<Image className='slider__text--shape__icon' src='/images/icons/text-shape-icon.png' alt='text-shape-icon' width={25} height={21} /> New Collection
-												</p>
-												<h2 className='slider__content--maintitle h1'>
-													The Great Fashion <br />
-													Collection 2022
-												</h2>
-												<p className='slider__content--desc desc2 d-sm-2-none mb-40 '>
-													Up To 40% Off Final Sale Items. <br />
-													Caught in the Moment!
-												</p>
-												<a className='primary__btn slider__btn' href='shop.html'>
-													Show Collection
-													<svg className='slider__btn--arrow__icon' xmlns='http://www.w3.org/2000/svg' width='20.2' height='12.2' viewBox='0 0 6.2 6.2'>
-														<path d='M7.1,4l-.546.546L8.716,6.713H4v.775H8.716L6.554,9.654,7.1,10.2,9.233,8.067,10.2,7.1Z' transform='translate(-4 -4)' fill='currentColor'></path>
-													</svg>
-												</a>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div className='swiper-slide '>
-						<div className='hero__slider--items home1__slider--bg three'>
-							<div className='container-fluid'>
-								<div className='row'>
-									<div className='col-lg-6 offset-lg-6'>
-										<div className='hero__slider--items__inner'>
-											<div className='slider__content text-center'>
-												<p className='slider__content--desc desc1 mb-15'>
-													<Image className='slider__text--shape__icon' src='/images/icons/text-shape-icon.png' alt='text-shape-icon' width={25} height={21} />
-													New Collection
-												</p>
-												<h2 className='slider__content--maintitle h1'>
-													The Great Fashion <br />
-													Collection 2022
-												</h2>
-												<p className='slider__content--desc desc2 d-sm-2-none mb-40'>
-													Up To 40% Off Final Sale Items. <br />
-													Caught in the Moment!
-												</p>
-												<a className='primary__btn slider__btn' href='shop.html'>
-													Show Collection
-													<svg className='slider__btn--arrow__icon' xmlns='http://www.w3.org/2000/svg' width='20.2' height='12.2' viewBox='0 0 6.2 6.2'>
-														<path d='M7.1,4l-.546.546L8.716,6.713H4v.775H8.716L6.554,9.654,7.1,10.2,9.233,8.067,10.2,7.1Z' transform='translate(-4 -4)' fill='currentColor'></path>
-													</svg>
-												</a>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div className='swiper__nav--btn swiper-button-next'></div>
-				<div className='swiper__nav--btn swiper-button-prev'></div>
-			</div>
-		</section>
-	)
-}
+// export default function Slider() {
+// 	return (
+// 		<section className='hero__slider--section'>
+// 			<div className='hero__slider--inner hero__slider--activation swiper'>
+// 				<div className='hero__slider--wrapper swiper-wrapper'>
+// 					<div className='swiper-slide '>
+// 						<div className='hero__slider--items home1__slider--bg'>
+// 							<div className='container-fluid'>
+// 								<div className='hero__slider--items__inner'>
+// 									<div className='row row-cols-1'>
+// 										<div className='col'>
+// 											<div className='slider__content'>
+// 												<p className='slider__content--desc desc1 mb-15'>
+// 													<Image className='slider__text--shape__icon' src='/images/icons/text-shape-icon.png' alt='text-shape-icon' width={25} height={21} /> New Collection
+// 												</p>
+// 												<h2 className='slider__content--maintitle h1'>
+// 													The Great Fashion <br />
+// 													Collection 2022
+// 												</h2>
+// 												<p className='slider__content--desc desc2 d-sm-2-none mb-40'>
+// 													Up To 40% Off Final Sale Items. <br />
+// 													Caught in the Moment!
+// 												</p>
+// 												<a className='slider__btn primary__btn' href='shop.html'>
+// 													Show Collection
+// 													<svg className='primary__btn--arrow__icon' xmlns='http://www.w3.org/2000/svg' width='20.2' height='12.2' viewBox='0 0 6.2 6.2'>
+// 														<path d='M7.1,4l-.546.546L8.716,6.713H4v.775H8.716L6.554,9.654,7.1,10.2,9.233,8.067,10.2,7.1Z' transform='translate(-4 -4)' fill='currentColor'></path>
+// 													</svg>
+// 												</a>
+// 											</div>
+// 										</div>
+// 									</div>
+// 								</div>
+// 							</div>
+// 						</div>
+// 					</div>
+// 					<div className='swiper-slide '>
+// 						<div className='hero__slider--items home1__slider--bg two'>
+// 							<div className='container-fluid'>
+// 								<div className='row'>
+// 									<div className='col'>
+// 										<div className='hero__slider--items__inner'>
+// 											<div className='slider__content'>
+// 												<p className='slider__content--desc desc1 mb-15'>
+// 													<Image className='slider__text--shape__icon' src='/images/icons/text-shape-icon.png' alt='text-shape-icon' width={25} height={21} /> New Collection
+// 												</p>
+// 												<h2 className='slider__content--maintitle h1'>
+// 													The Great Fashion <br />
+// 													Collection 2022
+// 												</h2>
+// 												<p className='slider__content--desc desc2 d-sm-2-none mb-40 '>
+// 													Up To 40% Off Final Sale Items. <br />
+// 													Caught in the Moment!
+// 												</p>
+// 												<a className='primary__btn slider__btn' href='shop.html'>
+// 													Show Collection
+// 													<svg className='slider__btn--arrow__icon' xmlns='http://www.w3.org/2000/svg' width='20.2' height='12.2' viewBox='0 0 6.2 6.2'>
+// 														<path d='M7.1,4l-.546.546L8.716,6.713H4v.775H8.716L6.554,9.654,7.1,10.2,9.233,8.067,10.2,7.1Z' transform='translate(-4 -4)' fill='currentColor'></path>
+// 													</svg>
+// 												</a>
+// 											</div>
+// 										</div>
+// 									</div>
+// 								</div>
+// 							</div>
+// 						</div>
+// 					</div>
+// 					<div className='swiper-slide '>
+// 						<div className='hero__slider--items home1__slider--bg three'>
+// 							<div className='container-fluid'>
+// 								<div className='row'>
+// 									<div className='col-lg-6 offset-lg-6'>
+// 										<div className='hero__slider--items__inner'>
+// 											<div className='slider__content text-center'>
+// 												<p className='slider__content--desc desc1 mb-15'>
+// 													<Image className='slider__text--shape__icon' src='/images/icons/text-shape-icon.png' alt='text-shape-icon' width={25} height={21} />
+// 													New Collection
+// 												</p>
+// 												<h2 className='slider__content--maintitle h1'>
+// 													The Great Fashion <br />
+// 													Collection 2022
+// 												</h2>
+// 												<p className='slider__content--desc desc2 d-sm-2-none mb-40'>
+// 													Up To 40% Off Final Sale Items. <br />
+// 													Caught in the Moment!
+// 												</p>
+// 												<a className='primary__btn slider__btn' href='shop.html'>
+// 													Show Collection
+// 													<svg className='slider__btn--arrow__icon' xmlns='http://www.w3.org/2000/svg' width='20.2' height='12.2' viewBox='0 0 6.2 6.2'>
+// 														<path d='M7.1,4l-.546.546L8.716,6.713H4v.775H8.716L6.554,9.654,7.1,10.2,9.233,8.067,10.2,7.1Z' transform='translate(-4 -4)' fill='currentColor'></path>
+// 													</svg>
+// 												</a>
+// 											</div>
+// 										</div>
+// 									</div>
+// 								</div>
+// 							</div>
+// 						</div>
+// 					</div>
+// 				</div>
+// 				<div className='swiper__nav--btn swiper-button-next'></div>
+// 				<div className='swiper__nav--btn swiper-button-prev'></div>
+// 			</div>
+// 		</section>
+// 	)
+// }
