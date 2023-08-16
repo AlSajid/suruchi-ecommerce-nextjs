b85e8114691fab6c1bcecf18bf1c9de49f58770e
Al Sajid <alsajiduzzaman@outlook.com>
2023-08-16 17:49:05 +0600

    cart route issue fixed

---
+++ app/cart/page.tsx
@@ -0,0 +1,17 @@
+import BrandLogo from '@/components/BrandLogo'
+import Products from '@/components/Products'
+import Shipping from '@/components/Shipping'
+import Breadcrumb from '@/components/Breadcrumb'
+import Cart from '@/app/cart/Cart'
+
+export default function Page() {
+	return (
+		<main className='main__content_wrapper'>
+			<Breadcrumb title='Shopping Cart' />
+			<Cart />
+			<Products title='New Products' />
+			<BrandLogo />
+			<Shipping />
+		</main>
+	)
+}
