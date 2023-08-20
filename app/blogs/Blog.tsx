90d5f0770b493ea96cf1c519020dc488639c1906
Al Sajid <alsajiduzzaman@outlook.com>
2023-08-20 15:39:21 +0600

    blogs page optimized

--- app/blogs/Blog.tsx
+++ app/blogs/Blog.tsx
@@ -1,128 +1,26 @@
+import Pagination from '@/components/Pagination'
 import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Blog() {
 	return (
-		<section className='blog__section section--padding'>
-			<div className='container'>
-				<div className='section__heading text-center mb-50'>
-					<h2 className='section__heading--maintitle'>From The Blog</h2>
+		<div className='col mb-30'>
+			<div className='blog__items'>
+				<div className='blog__thumbnail'>
+					<Link className='blog__thumbnail--link' href='blog-details'>
+						<Image className='blog__thumbnail--img' src='/images/blogs/blog1.png' alt='blog-img' width={360} height={313} />
+					</Link>
 				</div>
-
-				<div className='blog__section--inner'>
-					<div className='row row-cols-lg-3 row-cols-md-2 row-cols-sm-2 row-cols-sm-u-2 row-cols-1 mb--n30'>
-						<div className='col mb-30'>
-							<div className='blog__items'>
-								<div className='blog__thumbnail'>
-									<Link className='blog__thumbnail--link' href='blog-details'>
-										<Image className='blog__thumbnail--img' src='/images/blogs/blog1.png' alt='blog-img' width={360} height={313} />
-									</Link>
-								</div>
-								<div className='blog__content'>
-									<span className='blog__content--meta'>February 03, 2022</span>
-									<h3 className='blog__content--title'>
-										<Link href='blog-details'>Fashion Trends In 2021 Styles, Colors, Accessories</Link>
-									</h3>
-									<Link className='blog__content--btn primary__btn' href='blog-details'>
-										Read more
-									</Link>
-								</div>
-							</div>
-						</div>
-						<div className='col mb-30'>
-							<div className='blog__items'>
-								<div className='blog__thumbnail'>
-									<Link className='blog__thumbnail--link' href='blog-details'>
-										<Image className='blog__thumbnail--img' src='/images/blogs/blog2.png' alt='blog-img' width={360} height={313} />
-									</Link>
-								</div>
-								<div className='blog__content'>
-									<span className='blog__content--meta'>February 03, 2022</span>
-									<h3 className='blog__content--title'>
-										<Link href='blog-details'>Meet the Woman Behind Cool Ethical Label Reformation </Link>
-									</h3>
-									<Link className='blog__content--btn primary__btn' href='blog-details'>
-										Read more
-									</Link>
-								</div>
-							</div>
-						</div>
-						<div className='col mb-30'>
-							<div className='blog__items'>
-								<div className='blog__thumbnail'>
-									<Link className='blog__thumbnail--link' href='blog-details'>
-										<Image className='blog__thumbnail--img' src='/images/blogs/blog3.png' alt='blog-img' width={360} height={313} />
-									</Link>
-								</div>
-								<div className='blog__content'>
-									<span className='blog__content--meta'>February 03, 2022</span>
-									<h3 className='blog__content--title'>
-										<Link href='blog-details'>Lauryn Hill Could Make Tulle Skirt and Cowboy</Link>
-									</h3>
-									<Link className='blog__content--btn primary__btn' href='blog-details'>
-										Read more
-									</Link>
-								</div>
-							</div>
-						</div>
-						<div className='col mb-30'>
-							<div className='blog__items'>
-								<div className='blog__thumbnail'>
-									<Link className='blog__thumbnail--link' href='blog-details'>
-										<Image className='blog__thumbnail--img' src='/images/blogs/blog4.png' alt='blog-img' width={360} height={313} />
-									</Link>
-								</div>
-								<div className='blog__content'>
-									<span className='blog__content--meta'>February 03, 2022</span>
-									<h3 className='blog__content--title'>
-										<Link href='blog-details'>Consectetur adipisicing. magnam commodi doloribus.</Link>
-									</h3>
-									<Link className='blog__content--btn primary__btn' href='blog-details'>
-										Read more
-									</Link>
-								</div>
-							</div>
-						</div>
-						<div className='col mb-30'>
-							<div className='blog__items'>
-								<div className='blog__thumbnail'>
-									<Link className='blog__thumbnail--link' href='blog-details'>
-										<Image className='blog__thumbnail--img' src='/images/blogs/blog1.png' alt='blog-img' width={360} height={313} />
-									</Link>
-								</div>
-								<div className='blog__content'>
-									<span className='blog__content--meta'>February 03, 2022</span>
-									<h3 className='blog__content--title'>
-										<Link href='blog-details'>Lorem ipsum, dolor sit amet consectetur are elit.</Link>
-									</h3>
-									<Link className='blog__content--btn primary__btn' href='blog-details'>
-										Read more
-									</Link>
-								</div>
-							</div>
-						</div>
-						<div className='col mb-30'>
-							<div className='blog__items'>
-								<div className='blog__thumbnail'>
-									<Link className='blog__thumbnail--link' href='blog-details'>
-										<Image className='blog__thumbnail--img' src='/images/blogs/blog2.png' alt='blog-img' width={360} height={313} />
-									</Link>
-								</div>
-								<div className='blog__content'>
-									<span className='blog__content--meta'>February 03, 2022</span>
-									<h3 className='blog__content--title'>
-										<Link href='blog-details'>Meet the Woman Behind Cool Ethical Label Reformation </Link>
-									</h3>
-									<Link className='blog__content--btn primary__btn' href='blog-details'>
-										Read more
-									</Link>
-								</div>
-							</div>
-						</div>
-					</div>
-					
+				<div className='blog__content'>
+					<span className='blog__content--meta'>February 03, 2022</span>
+					<h3 className='blog__content--title'>
+						<Link href='blog-details'>Fashion Trends In 2021 Styles, Colors, Accessories</Link>
+					</h3>
+					<Link className='blog__content--btn primary__btn' href='blog-details'>
+						Read more
+					</Link>
 				</div>
 			</div>
-		</section>
+		</div>
 	)
 }
--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,10 +1,29 @@
 import ContentWrapper from '@/components/ContentWrapper'
-import Blogs from './Blog'
+import Pagination from '@/components/Pagination'
+import Blog from './Blog'
 
-export default function Blog() {
+export default function Blogs() {
+	const blogCount = 6
+	const blogs = new Array(blogCount).fill(null)
+
 	return (
 		<ContentWrapper title='Blog Details'>
-			<Blogs />
+			<section className='blog__section section--padding'>
+				<div className='container'>
+					<div className='section__heading text-center mb-50'>
+						<h2 className='section__heading--maintitle'>From The Blog</h2>
+					</div>
+
+					<div className='blog__section--inner'>
+						<div className='row row-cols-lg-3 row-cols-md-2 row-cols-sm-2 row-cols-sm-u-2 row-cols-1 mb--n30'>
+							{blogs.map((_, index) => (
+								<Blog key={index} />
+							))}
+						</div>
+						<Pagination />
+					</div>
+				</div>
+			</section>
 		</ContentWrapper>
 	)
 }
